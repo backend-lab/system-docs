66b013884a9dc11a96561db951128591270ba1ed
backend-lab <56413659+backend-lab@users.noreply.github.com>
2024-09-22 22:23:17 +0300

    Revert "Deploy website - based on 5116308b1073b98dd5b5d0c84f4f864a9c511153"

--- assets/js/a7456010.d82fc76b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksystem_docs=self.webpackChunksystem_docs||[]).push([[1235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
